1ff479727db97020ba74c80316ebbcd3fb2685a8
Nathaniel Nasarow <nasarow@epigno.jp>
2023-05-13 17:37:59 +0900

    [composables] useObservedProperty: Adds new types for observed properties
    Signed-off-by: Nathaniel Nasarow <nasarow@epigno.jp>

---
+++ sa-frontend/composables/useObservedProperty.ts
@@ -0,0 +1,14 @@
+export type ObservedPropertyType = {
+    "@iot.id": number
+    "@iot.selfLink": string
+    "Datastreams@iot.navigationLink": string
+    description: string
+    name: string
+    definition: string
+}
+
+export type ObservedPropertyUpdateType = {
+    description: string
+    name: string
+    definition: string
+}

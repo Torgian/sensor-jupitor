207811f202a40d9f3ca67133b4ac0ce9d274df48
Nathaniel Nasarow <nasarow@epigno.jp>
2023-05-13 18:01:30 +0900

    [composables] useFeatureOfInterest: Adds new types
    Signed-off-by: Nathaniel Nasarow <nasarow@epigno.jp>

---
+++ sa-frontend/composables/useFeatureOfInterest.ts
@@ -0,0 +1,22 @@
+export type FeatureOfInterestType = {
+    "@iot.id": number
+    "@iot.selfLink": string
+    "Observations@iot.navigationLink": string
+    name: string
+    description: string
+    encodingType: string
+    feature: FeatureType
+}
+
+// May update this to include enumerable types for geometry
+export type FeatureType = {
+    type: string
+    coordinates: number[]
+}
+
+export type FeatureOfInterestUpdateType = {
+    name: string
+    description: string
+    encodingType: string
+    feature: FeatureType
+}

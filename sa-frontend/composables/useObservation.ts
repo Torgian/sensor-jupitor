38cccc50ff97c78f38acc4f44e04a872a4228877
Nathaniel Nasarow <nasarow@epigno.jp>
2023-05-13 17:48:42 +0900

    [composables] useObservation: Adds new types for observations
    Signed-off-by: Nathaniel Nasarow <nasarow@epigno.jp>

---
+++ sa-frontend/composables/useObservation.ts
@@ -0,0 +1,16 @@
+// When updating or creating an observation, a Feature ID must be included in the URL
+export type ObservationType = {
+    "@iot.id": number
+    "@iot.selfLink": string
+    "FeatureOfInterest@iot.navigationLink": string
+    "Datastream@iot.navigationLink": string
+    phenomenonTime: Date
+    resultTime: Date
+    result: number
+}
+
+export type ObservationUpdateType = {
+    phenomenonTime: Date
+    resultTime: Date
+    result: number
+}
